974883f18e777695b81b7c5b08c7144d283a366e
matheus costa <matheuscosta.tech@gmail.com>
2022-10-25 07:52:21 -0300

    Playing sounds

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,25 @@ type SoundsType = {
 
 const sounds: SoundsType = {
     'w': () => {
-        new Audio('/sounds/tom1.mp3').play();
+        new Audio('sounds/tom-1.mp3').play();
     },
     'a': () => {
-        new Audio('/sounds/tom2.mp3').play();
+        new Audio('sounds/tom-2.mp3').play();
     },
     's': () => {
-        new Audio('/sounds/tom3.mp3').play();
+        new Audio('sounds/tom-3.mp3').play();
     },
     'd': () => {
-        new Audio('/sounds/tom4.mp3').play();
+        new Audio('sounds/tom-4.mp3').play();
     },
     'j': () => {
-        new Audio('/sounds/snare.mp3').play();
+        new Audio('sounds/snare.mp3').play();
     },
     'k': () => {
-        new Audio('/sounds/crash.mp3').play();
+        new Audio('sounds/crash.mp3').play();
     },
     'l': () => {
-        new Audio('/sounds/kick.mp3').play();
+        new Audio('sounds/kick-bass.mp3').play();
     },
 };
 
