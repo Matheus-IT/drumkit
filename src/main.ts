da1bf0a660c8b6111d3698554478ddd349caa853
matheus costa <matheuscosta.tech@gmail.com>
2022-12-26 20:36:13 -0300

    Playing drums with keyboard

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,15 @@
 type SoundsType = {
-    [key: string]: () => void
+    [key: string]: () => HTMLAudioElement
 };
 
 const sounds: SoundsType = {
-    'w': () => {
-        new Audio('sounds/tom-1.mp3').play();
-    },
-    'a': () => {
-        new Audio('sounds/tom-2.mp3').play();
-    },
-    's': () => {
-        new Audio('sounds/tom-3.mp3').play();
-    },
-    'd': () => {
-        new Audio('sounds/tom-4.mp3').play();
-    },
-    'j': () => {
-        new Audio('sounds/snare.mp3').play();
-    },
-    'k': () => {
-        new Audio('sounds/crash.mp3').play();
-    },
-    'l': () => {
-        new Audio('sounds/kick-bass.mp3').play();
-    },
+    'w': () => new Audio('sounds/tom-1.mp3'),
+    'a': () => new Audio('sounds/tom-2.mp3'),
+    's': () => new Audio('sounds/tom-3.mp3'),
+    'd': () => new Audio('sounds/tom-4.mp3'),
+    'j': () => new Audio('sounds/snare.mp3'),
+    'k': () => new Audio('sounds/crash.mp3'),
+    'l': () => new Audio('sounds/kick-bass.mp3'),
 };
 
 const drumSet = document.querySelectorAll('.drum');
@@ -31,8 +17,14 @@ const drumSet = document.querySelectorAll('.drum');
 drumSet.forEach(e => {
     e.addEventListener('click', (e) => {
         const target = e.target as HTMLButtonElement;
-        sounds[target.innerText]();
+        const audioElement = sounds[target.innerText]();
+        audioElement.play();
     });
+});
+
+document.addEventListener('keypress', function(event) {
+    const audioElement = sounds[event.key]();
+    audioElement.play();
 });
 
 export {}; // needed to make it a module
